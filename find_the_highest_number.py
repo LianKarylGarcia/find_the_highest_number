bc37962f6a725840d6d6c3890f79953d4d356610
LianKarylGarcia <liankarylg@gmail.com>
2024-10-18 20:57:03 +0800

    This will allow users to input 5 numbers

--- find_the_highest_number.py
+++ find_the_highest_number.py
@@ -1,5 +1,10 @@
 
 # Input variables 
 num1 = float(input("Input first number: "))
+num2 = float(input("Input second number: "))
+num3 = float(input("Input third number: "))
+num4 = float(input("Input fourth number: "))
+num5 = float(input("Input fifth number: "))
+
 # Compare  variables to each other
 # Print what variable is the highest number

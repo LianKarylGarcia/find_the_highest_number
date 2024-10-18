95492c2a4003b0effb881b3b83d852bfcdd92659
LianKarylGarcia <liankarylg@gmail.com>
2024-10-18 21:09:40 +0800

    Using elif statement to compare num2, num3, and num4

--- find_the_highest_number.py
+++ find_the_highest_number.py
@@ -11,6 +11,15 @@ def find_the_highest_number(num1,num2,num3,num4,num5):
     if num1>num2 and num1>num3 and num1>num4 and num1>num5:
         print("The first number is the highest number!!!")
         return num1
+    elif num2>num1 and num2>num3 and num2>num4 and num2>num5:
+        print("The second number is the highest number!!!")
+        return num2
+    elif num3>num1 and num3>num2 and num3>num4 and num3>num5:
+        print("The third number is the highest number!!!")
+        return num3
+    elif num4>num1 and num4>num2 and num4>num3 and num4>num5:
+        print("The fourth number is the highest number!!!")
+        return num4
     
 
 # Print what variable is the highest number

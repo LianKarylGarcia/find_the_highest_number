fdcf0c021b3377661e6888038b5151cffa2e7aee
LianKarylGarcia <liankarylg@gmail.com>
2024-10-18 23:05:14 +0800

    The code is working correctly. First, I input five numbers, then define a function that sets num1 as the initial highest number. I use if statements to compare the numbers and find the highest. After that, I call the function to get the result and print the highest number. This approach works whether or not some numbers are the same.

--- find_the_highest_number.py
+++ find_the_highest_number.py
@@ -6,18 +6,34 @@ num3 = float(input("Input third number: "))
 num4 = float(input("Input fourth number: "))
 num5 = float(input("Input fifth number: "))
 
-# Initial highest number
-highest = num1
-
 # Compare  variables to each other
-if num2>highest:
-    highest = num2
-elif num3>highest:
-    highest = num3
-elif num4>highest:
-    highest = num4
-elif num5>highest:
-    highest = num5
-    
-# Print what variable is the highest number
-print(f"The highest number is: {highest}")
+def find_the_highest_number(num1,num2,num3,num4,num5):
+    highest=num1 # initial highest
+
+    if num2>highest:
+        highest=num2
+    else: 
+        highest=num1
+
+    if num3>highest:
+        highest=num3
+    else:
+        highest=num2
+
+    if num4>highest:
+        highest=num4
+    else:
+        highest=num3
+
+    if num5>highest:
+        highest=num5
+    else: 
+        highest=num4
+        
+    return highest
+
+# To call the function we used
+highest_number = find_the_highest_number(num1,num2,num3,num4,num5)
+
+# To print the highest number
+print(f"The highest number is: {highest_number}")

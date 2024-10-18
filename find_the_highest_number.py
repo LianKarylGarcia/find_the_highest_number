8a098600abd9be4f0b4083b12fd8739fa85859d8
LianKarylGarcia <liankarylg@gmail.com>
2024-10-18 21:35:37 +0800

    Using a different approach because the previous code does not work when there are duplicate numbers. This approach allows us to determine which number is the highest, even if some numbers are the same.

--- find_the_highest_number.py
+++ find_the_highest_number.py
@@ -6,22 +6,25 @@ num3 = float(input("Input third number: "))
 num4 = float(input("Input fourth number: "))
 num5 = float(input("Input fifth number: "))
 
+# Initial highest number
+highest = num1
+
 # Compare  variables to each other
 def find_the_highest_number(num1,num2,num3,num4,num5):
-    if num1>num2 and num1>num3 and num1>num4 and num1>num5:
-        print("The first number is the highest number!!!")
-        return num1
-    elif num2>num1 and num2>num3 and num2>num4 and num2>num5:
+    if num2>highest:
         print("The second number is the highest number!!!")
         return num2
-    elif num3>num1 and num3>num2 and num3>num4 and num3>num5:
+    elif num3>highest:
         print("The third number is the highest number!!!")
         return num3
-    elif num4>num1 and num4>num2 and num4>num3 and num4>num5:
+    elif num4>highest:
         print("The fourth number is the highest number!!!")
         return num4
-    else:
+    elif num5>highest:
         print("The fifth number is the highest number!!!")
+        return num5
+    else: 
+        print("The first number is the highest number!!!")
     
 
 # Print what variable is the highest number

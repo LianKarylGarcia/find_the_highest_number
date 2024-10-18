77491a17860a2072103ce0b6766e446ef4408264
LianKarylGarcia <liankarylg@gmail.com>
2024-10-18 21:00:05 +0800

    Defining the function that i'll be using to find the highest numumber

--- find_the_highest_number.py
+++ find_the_highest_number.py
@@ -7,4 +7,7 @@ num4 = float(input("Input fourth number: "))
 num5 = float(input("Input fifth number: "))
 
 # Compare  variables to each other
+def find_the_highest_number(num1,num2,num3,num4,num5):
+    
+
 # Print what variable is the highest number
